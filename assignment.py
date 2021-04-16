9fa32f5e67054d827032b900d7b25f5f4eee4336
mbaiza27 <mbaiza27@outlook.com>
2021-04-16 15:43:50 -0700

    Started BFS, and added minor functionality to get started on generating successor nodes

--- assignment.py
+++ assignment.py
@@ -4,6 +4,9 @@ import sys
 import math
 import csv
 
+#Global Counters 
+expansionCounter = 0
+
 #Generic file handling function to read the two state files
 def fileStuff(filepath):
     f = open(filepath,"r")
@@ -24,11 +27,75 @@ def entry():
     outputFile = sys.argv[4]
     return initialState, finalState, outputFile
 
+
+def generateSuccessors(node):
+    print("Generating Successors...")
+    global expansionCounter
+    successors = []
+
+    
+    #First we have to find out which side the boat is on
+    #node[0] is left bank
+    #node[1] is right bank
+
+    #node[0][0] is left bank chickens
+    left_chickens = node[0][0]
+    #node[1][0]is right bank chickens
+    right_chickens = node[1][0]
+    #node[0][1] is left bank wolves
+    left_wolves = node[0][1]
+    #node [1][1] is right bank wolves
+    right_wolves = node[1][1]
+
+    #Find where boat is
+    is_boat_right_bank = True
+    
+
+    if node[1][2] == 1:
+        is_boat_right_bank = True
+        boat_bank = node[1]
+    else:
+        is_boat_right_bank = False
+        boat_bank = node[0]
+
+            
+    print("Boat is in Right Bank:", is_boat_right_bank, " Boat:", boat_bank)
+    #1)Put one chicken in the boat
+
+    #2)Put two chickens in the boat
+
+    #3)Put one wolf in the boat
+
+    #4)Put one wolf and one chicken in the boat
+
+    #5)Put two wolves in the boat
+
+    expansionCounter += 1
+    return successors
+
 def bfs(initialState, finalState, outputFile):
     print("Running bfs...")
+    print("Starting State:", initialState)
+
+    #Establishes a queue of nodes
+    nodeList = [initialState]
+    currentNode = initialState
+
+    while True:
+        if currentNode == finalState:
+            #Break out of the infinite loop because we have reached the solution
+            break
+        else:
+            newNode = nodeList.pop(0)
+
+            successors = generateSuccessors(newNode)
+            
+            break
+
 
 def dfs(initialState, finalState, outputFile):
     print("Running dfs...")
+    
 
 def iddfs(initialState, finalState, outputFile):
     print("Running iddfs...")
